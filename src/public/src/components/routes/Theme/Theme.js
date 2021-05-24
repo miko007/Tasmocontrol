d893a89d1ecc8fdf6fbfed92d73577fa1a34d095
Michael Ochmann <michael.ochmann@propeller.de>
2021-05-25 00:52:43 +0200

    added theme color documentation

--- src/public/src/components/routes/Theme/Theme.js
+++ src/public/src/components/routes/Theme/Theme.js
@@ -2,6 +2,28 @@ import React, {useState, useMemo, useContext} from "react";
 
 import DeviceContext from "../../context/DeviceContext";
 
+/**
+ * Theme Color assignments as of Tasmota code documentation:
+ *
+ * #define COLOR_TEXT                  "#000"       // [WebColor1] Global text color - Black
+ * #define COLOR_BACKGROUND            "#fff"       // [WebColor2] Global background color - White
+ * #define COLOR_FORM                  "#f2f2f2"    // [WebColor3] Form background color - Greyish
+ * #define COLOR_INPUT_TEXT            "#000"       // [WebColor4] Input text color - Black
+ * #define COLOR_INPUT                 "#fff"       // [WebColor5] Input background color - White
+ * #define COLOR_CONSOLE_TEXT          "#000"       // [WebColor6] Console text color - Black
+ * #define COLOR_CONSOLE               "#fff"       // [WebColor7] Console background color - White
+ * #define COLOR_TEXT_WARNING          "#f00"       // [WebColor8] Warning text color - Red
+ * #define COLOR_TEXT_SUCCESS          "#008000"    // [WebColor9] Success text color - Green
+ * #define COLOR_BUTTON_TEXT           "#fff"       // [WebColor10] Button text color - White
+ * #define COLOR_BUTTON                "#1fa3ec"    // [WebColor11] Button color - Blueish
+ * #define COLOR_BUTTON_HOVER          "#0e70a4"    // [WebColor12] Button color when hovered over - Darker blueish
+ * #define COLOR_BUTTON_RESET          "#d43535"    // [WebColor13] Restart/Reset/Delete button color - Redish
+ * #define COLOR_BUTTON_RESET_HOVER    "#931f1f"    // [WebColor14] Restart/Reset/Delete button color when hovered over - Darker redish
+ * #define COLOR_BUTTON_SAVE           "#47c266"    // [WebColor15] Save button color - Greenish
+ * #define COLOR_BUTTON_SAVE_HOVER     "#5aaf6f"    // [WebColor16] Save button color when hovered over - Darker greenish
+ * #define COLOR_TIMER_TAB_TEXT        "#fff"       // [WebColor17] Config timer tab text color - White
+ * #define COLOR_TIMER_TAB_BACKGROUND  "#999"       // [WebColor18] Config timer tab background color - Light grey
+ */
 const Themes = [
 	{
 		name   : "Dark",
@@ -50,18 +72,18 @@ const Theme = () => {
 							{Themes.map((theme, index) => <option value={index} key={index}>{theme.name}</option>)}
 						</select>
 					</p>
-					<ul className="list-group">
+					<ul className="list-group small">
 						{Object.values(devices).map((device, index) => {
 							return (
 								<li className="list-group-item" key={index}>
-									<input type="checkbox" style={{verticalAlign : "middle", marginRight : "0.5rem"}} />
-									<span style={{verticalAlign: "middle"}}>{device.Status.FriendlyName}</span>
+									<input type="checkbox" id={`dev_${index}`} />
+									<label htmlFor={`dev_${index}`}>{device.Status.FriendlyName}</label>
 								</li>
 							);
 						})}
 					</ul>
 					<p>
-						<button className="btn btn-primary btn-large pull-right">update all</button>
+						<button className="btn btn-warning btn-large pull-right">update all</button>
 					</p>
 				</section>
 				<section className="preview" style={{color: colors[0], background: colors[1]}}>
